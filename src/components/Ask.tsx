4e585bea5fb2f0da16d55f36cfe009ae257b5671
Qtipy <roman.dilmi@orange.fr>
2021-05-28 00:19:45 +0200

    Add asnwer structure and score

--- src/components/Ask.tsx
+++ src/components/Ask.tsx
@@ -1,43 +1,85 @@
-import {useState} from 'react';
+import { FormControl, FormControlLabel, Radio, RadioGroup } from '@material-ui/core';
+import { useState } from 'react';
+import { debounce } from '../helpers/data';
+
+interface Answer {
+    id: number;
+    answer: string;
+    points: number;
+}
 
 interface Question {
     question: string;
-    answerChoices: string[];
+    answerChoices: Answer[];
 }
+
+const debounceTimer: number = 200;
 
 export const Ask = () => {
     const datas: Question[] = [
         {
             question: 'Bac +5 équivalent ou supérieur ?',
-            answerChoices: ['Oui', 'Non']
+            answerChoices: [{id: 1, answer: 'Oui', points: 10}, {id: 2, answer: 'Non', points: 0}]
         },
         {
             question: 'En couple',
-            answerChoices: ['Ouii', 'Nonn']
+            answerChoices: [{id: 3, answer: 'Oui', points: 10}, {id: 4, answer: 'Non', points: 0}]
         },
         {
             question: 'Domicile',
-            answerChoices: ['Appartement', 'Maison', 'Autres']
+            answerChoices: [
+                {id: 5, answer: 'Appartement', points: 10},
+                {id: 6, answer: 'Maison', points: 10},
+                {id: 7, answer: 'Palace', points: 30},
+            ]
         },
     ];
 
     const [index, setIndex] = useState<number>(0);
+    const [value, setValue] = useState<number | undefined>(undefined);
+    const [points, setPoints] = useState<number>(0);
+    const [showScore, setShowScore] = useState<boolean>(false);
 
-    const nextQuestion = () => {
-        index === datas.length - 1 ? setIndex(0) : setIndex(index + 1);
+    // const nextQuest = debounce(() => {
+    //     index === datas.length - 1 ? setIndex(0) : setIndex(index + 1);
+    // }, 200);
+
+    const nextQuest = debounce(() => {
+        setIndex(index + 1);
+    }, debounceTimer);
+
+    const handleOnChange = (answer: Answer) => {
+        if (index === datas.length - 1) {
+            setPoints(points + answer.points);
+            setIndex(0);
+            setShowScore(true);
+        } else {
+            setValue(answer.id);
+            setPoints(points + answer.points);
+            nextQuest();
+        }
     };
     
     return (
-        <form>
-            <p>{datas[index].question}</p>
-            {datas[index].answerChoices.map(q => {
-               return (
-                <div onClick={nextQuestion}>
-                    <input type="radio" id={q} name={q}/>
-                    <label htmlFor={q}>{q}</label>
-                </div>)
-            }
-            )}
-        </form>
+        <>
+            {showScore && <>
+                Bien joué, tu as {points} points !
+            </>}
+            {!showScore &&
+            <>
+                <p>{datas[index].question}</p>
+                <FormControl component="fieldset" >
+                    {datas[index].answerChoices.map((a) => {
+                        return (
+                            <div key={a.id}>
+                                <RadioGroup name={datas[index].question} value={value} onChange={() => handleOnChange(a)}>
+                                    <FormControlLabel value={a.id} control={<Radio />} label={a.answer} />
+                                </RadioGroup>
+                            </div>
+                        )}
+                    )}
+                </FormControl>
+            </>}
+    </>
     );
 };
